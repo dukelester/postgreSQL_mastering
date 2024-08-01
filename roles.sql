4e0687fffb4810df4f5d1438f58b174763d82d20
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-08-01 10:28:38 +0300

    alter role

--- roles.sql
+++ roles.sql
@@ -13,3 +13,14 @@ CREATE ROLE nikhil superuser login PASSWORD 'duke2030';
 create role lester superuser login password 'lester2030';
 
 CREATE ROLE api login PASSWORD 'securepass1' CONNECTION LIMIT 1000;
+
+-- ALTER ROLE
+-- ALTER ROLE role_name [WITH] option;
+
+CREATE ROLE ravi login PASSWORD 'geeks12345';
+
+ALTER ROLE ravi superuser;
+
+ALTER ROLE ravi VALID UNTIL '2024-10-08';
+
+ALTER ROLE ravi CREATEDB;
