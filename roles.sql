49d7d337980f3c4217ef0978f179d04f5876f6bb
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-08-01 11:35:12 +0300

    The grant

--- roles.sql
+++ roles.sql
@@ -35,3 +35,42 @@ DROP ROLE if EXISTS test;
 DROP ROLE if EXISTS role_1;
 
 DROP ROLE if EXISTS user_one, nikhil;
+
+-- GRANT
+-- GRANT statement is a powerful tool used to assign privileges to a role, allowing it to alter database objects like tables, views, functions, and more.
+/* Syntax
+GRANT privilege_list | ALL 
+ON  table_name
+TO  role_name;
+*/
+
+CREATE ROLE anshul login PASSWORD 'duke2030';
+
+
+CREATE Table players (
+    player_id INT generated always as identity,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(20) NOT NULL UNIQUE,
+    PRIMARY KEY(player_id)
+);
+
+SELECT * FROM players;
+
+-- grant select prevellages
+
+GRANT SELECT ON players to anshul;
+INSERT INTO players(first_name, last_name, email, phone)
+VALUES('raju', 'kumar', 'raju.kumar@geeksforgeeks.org', '408-111-2222');
+
+
+GRANT INSERT, UPDATE, DELETE ON players TO anshul;
+
+INSERT INTO players(first_name, last_name, email, phone)
+VALUES('kenny', 'mokie', 'jas.kumar@lester.org', '408-333-2222');
+
+INSERT INTO players(first_name, last_name, email, phone)
+VALUES('mokie', 'mary', 'mary.jase@lester.org', '408-787-2292');
+
+CREATE ROLE dlester login PASSWORD 'lester';
